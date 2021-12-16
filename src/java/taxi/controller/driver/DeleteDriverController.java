968fe9326723b987b2c6561ee6c69d1b5d3972fa
SarumanIsAlive <90073141+SarumanIsAlive@users.noreply.github.com>
2021-12-16 11:09:19 +0200

    README file+ v3

--- src/java/taxi/controller/driver/DeleteDriverController.java
+++ src/java/taxi/controller/driver/DeleteDriverController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DeleteDriverController extends HttpServlet {
-    private static final String ALL_DRIVERS_URL = "/drivers/all";
+    private static final String ALL_DRIVERS_URL = "/drivers/";
     private static final Injector injector = Injector.getInstance("taxi");
     private final DriverService driverService = (DriverService) injector
             .getInstance(DriverService.class);
